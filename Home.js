9ec519336df792755798a9b409ad3ef2b0f56225
Naomi Greenberg <ngreenberg@luc.edu>
2023-10-26 22:38:49 -0500

    basic html and css to start with

--- Home.js
+++ Home.js
@@ -1,1 +1,12 @@
+const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
 
+const image = new Image();
+
+image.onload = function() {
+    // Once the image is loaded, draw it on the canvas
+    ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+};
+
+// Set the image source to the path of your image
+image.src = '/Images/Portfolio3.jpg';
