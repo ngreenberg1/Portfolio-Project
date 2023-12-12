ad6c924db2f925a066cfe568e393b692966bea87
Naomi Greenberg <ngreenberg@luc.edu>
2023-12-12 10:59:10 -0600

    added slideshow with lazy loading

--- Home.js
+++ Home.js
@@ -73,3 +73,55 @@ document.addEventListener("DOMContentLoaded", function() {
     });
   });
 });
+
+let slideIndex = 1;
+
+// Initialize Intersection Observer
+const options = {
+  root: null,
+  rootMargin: '0px',
+  threshold: 0.5 // Adjust the threshold as needed
+};
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      console.log('Element is intersecting! Lazy loading triggered.');
+      showSlides(slideIndex);
+      observer.unobserve(entry.target);
+    }
+  });
+}, options);
+
+// Call observe on each slide
+const slides = document.getElementsByClassName("mySlides");
+for (let slide of slides) {
+  observer.observe(slide);
+}
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none"; 
+  }
+  slides[slideIndex-1].style.display = "block"; 
+}
+
+// Document Ready - Initial call to showSlides after Intersection Observer has observed the slides
+document.addEventListener("DOMContentLoaded", function() {
+  observer.observe(slides[0]); // Assuming the first slide is initially in the viewport
+});
